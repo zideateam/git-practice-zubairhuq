42eae1bc0b31452dcca237852e5389b984876b25
Zubair Mahmudul Huq <zubairmhuq@gmail.com>
2026-06-13 15:22:18 +0600

    Add basic error handling

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from datetime import date
-from utils import add, subtract, multiply
+from utils import add, subtract, multiply, divide
 
 print("My Name: Zubair Mahmudul Huq")
 print("Today's Date:", date.today())
@@ -7,3 +7,6 @@ print("Today's Date:", date.today())
 print("Addition:", add(10, 5))
 print("Subtraction:", subtract(10, 5))
 print("Multiplication:", multiply(10, 5))
+
+print("Division:", divide(10, 2))
+print("Division:", divide(10, 0))
--- src/utils.py
+++ src/utils.py
@@ -8,3 +8,9 @@ def subtract(a, b):
 
 def multiply(a, b):
     return a * b
+
+
+def divide(a, b):
+    if b == 0:
+        return "Error: Division by zero is not allowed."
+    return a / b
