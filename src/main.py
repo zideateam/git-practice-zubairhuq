9f5bdaad350b3803183e94de07b3f5bdb4bf25cc
Zubair Mahmudul Huq <zubairmhuq@gmail.com>
2026-06-13 15:11:00 +0600

    Add multiplication function

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 from datetime import date
-from utils import add, subtract
+from utils import add, subtract, multiply
 
-print("My Name: Zubair Huq")
+print("My Name: Zubair Mahmudul Huq")
 print("Today's Date:", date.today())
 
-print("Addition:", add(14, 23))
-print("Subtraction:", subtract(23, 18))
+print("Addition:", add(10, 5))
+print("Subtraction:", subtract(10, 5))
+print("Multiplication:", multiply(10, 5))
--- src/utils.py
+++ src/utils.py
@@ -4,3 +4,7 @@ def add(a, b):
 
 def subtract(a, b):
     return a - b
+
+
+def multiply(a, b):
+    return a * b
