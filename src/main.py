50461e12abf510cb2f644637ec9b56b6c49600be
Zubair Mahmudul Huq <zubairmhuq@gmail.com>
2026-06-13 14:08:14 +0600

    Add basic calculator functions

--- src/main.py
+++ src/main.py
@@ -1,4 +1,8 @@
 from datetime import date
+from utils import add, subtract
 
-print("My Name: Zubair Mahmudul Huq")
+print("My Name: Zubair Huq")
 print("Today's Date:", date.today())
+
+print("Addition:", add(14, 23))
+print("Subtraction:", subtract(23, 18))
